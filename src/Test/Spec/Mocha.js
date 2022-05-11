86a72be96cecf9842af452f27a647427123e2a14
Maciej Bielecki <zyla@prati.pl>
2022-05-11 18:17:55 +0000

    Update for purs 0.15

--- src/Test/Spec/Mocha.js
+++ src/Test/Spec/Mocha.js
@@ -2,52 +2,56 @@
 
 // module Test.Spec.Mocha
 
-if (typeof describe !== 'function' || typeof it !== 'function') {
-    throw new Error('Mocha globals seem to be unavailable!');
+if (typeof describe !== "function" || typeof it !== "function") {
+  throw new Error("Mocha globals seem to be unavailable!");
 }
 
-exports.itAsync = function (only) {
-    "use strict";
-    return function (name) {
-        return function (run) {
-            return function () {
-                var f = only ? it.only : it;
-                f(name, function (done) {
-                    return run(function () {
-                        done();
-                        return function () {};
-                    })(function (err) {
-                        done(err);
-                        return function () {};
-                    })();
-                });
-            };
-        };
+function itAsync_(only) {
+  "use strict";
+  return function (name) {
+    return function (run) {
+      return function () {
+        var f = only ? it.only : it;
+        f(name, function (done) {
+          return run(function () {
+            done();
+            return function () {};
+          })(function (err) {
+            done(err);
+            return function () {};
+          })();
+        });
+      };
     };
-};
+  };
+}
 
-exports.itPending = function (name) {
-    "use strict";
-    return function () {
-        it(name);
+export { itAsync_ as itAsync };
+
+export function itPending(name) {
+  "use strict";
+  return function () {
+    it(name);
+  };
+}
+
+function describe_(only) {
+  "use strict";
+  return function (name) {
+    return function (nested) {
+      return function () {
+        var f = only ? describe.only : describe;
+        f(name, function () {
+          nested();
+        });
+      };
     };
-};
+  };
+}
 
-exports.describe = function (only) {
-    "use strict";
-    return function (name) {
-        return function (nested) {
-            return function () {
-                var f = only ? describe.only : describe;
-                f(name, function () {
-                    nested();
-                });
-            };
-        };
-    };
-};
+export { describe_ as describe };
 
-exports.afterAsync = function (name) {
+export function afterAsync(name) {
   "use strict";
   return function (run) {
     return function () {
@@ -62,4 +66,4 @@ exports.afterAsync = function (name) {
       });
     };
   };
-};
+}
